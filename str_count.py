c4366515133ef265e23a2a5262797d69ae189670
qianyanxismile <34147598+qianyanxismile@users.noreply.github.com>
2018-12-05 07:56:51 +0800

    提交优化代码
    by jack

--- str_count.py
+++ str_count.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from functools import reduce
+
+
+def count_with_reduce(d):
+    """使用reduce统计"""
+    return reduce(lambda a, b: a+b, map(lambda item: len(list(filter(lambda kk: kk!='0', item))), d.values()))
+
+
+def str_count_optimization(d):
+    """str_count优化版本，使用计数方式统计"""
+    count = 0
+    for v in d.values():
+        for i n v:
+            if i != '0':
+                count += 1
+    return count
+
 
 def str_count(d):
     l=[]
